8ac3ba115a21f989edd0d8a1b9358ba230c3f6b5
Siddhant Patil <siddhantpatil208@gmail.com>
2020-05-22 19:42:05 +0530

    checking if the inverse is already calculated

--- cachematrix.R
+++ cachematrix.R
@@ -10,12 +10,19 @@ makeCacheMatrix <- function(x = matrix()) {
   setinv = function(inverse) inv <<- inverse 
   getinv = function() inv
   list(set=set, get=get, setinv=setinv, getinv=getinv)
-  
-
 }
 
 
 
 cacheSolve <- function(x, ...) {
+  inv = x$getinv()
+  
+  # if the inverse has already been calculated
+  if (!is.null(inv)){
+    # get it from the cache and skips the computation. 
+    message("getting cached data")
+    return(inv)
+  
+    }
        
 }
